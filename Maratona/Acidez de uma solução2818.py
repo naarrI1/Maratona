6dc11451db665fead90dce4afd5e5e6db1b7c985
iran <iransantana2624@gmail.com>
2024-09-30 15:21:38 -0300

    Update Acidez de uma solução2818.py

--- Maratona/Acidez de uma solução2818.py
+++ Maratona/Acidez de uma solução2818.py
@@ -1,11 +1,12 @@
-    ph = int(input("Digite o pH da solucao:"))
+ph = float(input("Digite o pH da solucao:\n"))
 
-    if ph = 7:
-        print("a solução é neutra")
-    if ph < 7:
-        print("a solução é ácida")
-    if ph > 7:
-        print("a solução é básica")
+if ph < 0 or ph >14:
+    print("Valor do pH deve estar entre 0 e 14")
 
-    if ph < 0 or ph >14:
-        print("Valor do pH deve estar entre 0 e 14")
+else:
+    if ph == 7.0:
+      print("Solucao neutra") 
+    elif ph < 7:
+      print("Solucao acida")
+    elif ph > 7:
+      print("Solucao basica")
